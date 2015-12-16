5d8a435d0d6ae6dd643102d3c736debbc26a7362
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-16 14:49:26 -0800

    Attempted Seed

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :user
 end
