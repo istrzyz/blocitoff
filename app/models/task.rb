73823e0985aa3728b011316552779628b286b035
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-17 09:06:41 -0800

    Commit to make new branch

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,3 @@
 class Task < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   belongs_to :user
 end
